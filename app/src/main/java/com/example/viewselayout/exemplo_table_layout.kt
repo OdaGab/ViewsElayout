e53cd314b5c30517b5cc85c71448ec1a40981fdc
Odair <odavita32@gmail.com>
2025-12-31 12:37:41 -0300

    TableLayout, TableRow e LinearLayout

--- app/src/main/java/com/example/viewselayout/exemplo_table_layout.kt
+++ app/src/main/java/com/example/viewselayout/exemplo_table_layout.kt
@@ -1,28 +1,20 @@
 package com.example.viewselayout
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class ActivityPeso2 : AppCompatActivity() {
+class exemplo_table_layout : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_peso2)
+        setContentView(R.layout.activity_exemplo_table_layout)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
-        val btnNext3 = findViewById<Button>(R.id.btnNext3)
-
-        btnNext3.setOnClickListener {
-            val intent = Intent(this, exemplo_table_layout::class.java)
-            startActivity(intent)
         }
     }
 }
