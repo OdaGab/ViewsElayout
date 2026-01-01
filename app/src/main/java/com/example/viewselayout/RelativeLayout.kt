fb5d5e3247b1dc074529c215ec7d9b0949762b61
Odair <odavita32@gmail.com>
2026-01-01 15:24:28 -0300

    RelativeLayout e possicionamento dos objetos

--- app/src/main/java/com/example/viewselayout/RelativeLayout.kt
+++ app/src/main/java/com/example/viewselayout/RelativeLayout.kt
@@ -1,29 +1,21 @@
 package com.example.viewselayout
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class ActivityPeso2 : AppCompatActivity() {
+class RelativeLayout : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_peso2)
+        setContentView(R.layout.activity_relative_layout)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
 
-        val btnNextPgFrame = findViewById<Button>(R.id.btnNextpgframe)
-
-        btnNextPgFrame.setOnClickListener {
-            val intent = Intent(this, FrameLayoutPregressbar::class.java)
-            startActivity(intent)
-        }
-    }
+            }
 }
