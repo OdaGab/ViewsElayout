b8874d086719a56d3628b220a026adbd2af317b6
Odair <odavita32@gmail.com>
2026-01-01 23:14:14 -0300

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	app/src/main/AndroidManifest.xml
    #	app/src/main/java/com/example/viewselayout/exemplo_table_layout.kt
    #	app/src/main/res/values/strings.xml

--- app/src/main/java/com/example/viewselayout/ScrollView.kt
+++ app/src/main/java/com/example/viewselayout/ScrollView.kt
@@ -1,28 +1,20 @@
 package com.example.viewselayout
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class RelativeLayout : AppCompatActivity() {
+class ScrollView : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_relative_layout)
+        setContentView(R.layout.activity_scroll_view)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
-
-        val btnNext5 = findViewById<Button>(R.id.btnNext5)
-        btnNext5.setOnClickListener {
-            val intent = Intent(this, ScrollView::class.java)
-            startActivity(intent)
         }
     }
 }
